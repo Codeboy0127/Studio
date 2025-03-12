35a30da57abc7cc102a2cb6c54be5a4544224e73
Codeboy0127 <codeboy.coder@outlook.com>
2025-03-12 16:06:35 -0700

    install localize

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
