ff42078eb749b93910545101d9517765d54ebfbc
Codeboy0127 <codeboy.coder@outlook.com>
2025-03-16 12:14:58 -0800

    add datapicker

--- src/app/page/main/main.component.ts
+++ src/app/page/main/main.component.ts
@@ -1,10 +1,16 @@
-import { Component,ViewEncapsulation } from '@angular/core';
+import { Component,ViewEncapsulation,ChangeDetectionStrategy, viewChild } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatIconModule} from '@angular/material/icon';
 import { EquipmentComponent } from "../../features/component/equipment/equipment.component";
 import { TranslateModule } from '@ngx-translate/core';
+
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-main',
@@ -14,10 +20,18 @@ import { TranslateModule } from '@ngx-translate/core';
     MatBadgeModule,
     MatIconModule,
     EquipmentComponent,
-    TranslateModule
+    TranslateModule,
+
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
 ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
 export class MainComponent {
